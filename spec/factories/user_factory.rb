0ac35aff9613201ee80a64b9efff684670db8989
a-velitsky <a.velitsky@gmail.com>
2017-02-22 05:18:55 -0500

    6.4 factory_girl added, user factory added (fix)

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -6,9 +6,12 @@ FactoryGirl.define do
 		first_name "Peter"
 		last_name "Example"
 		admin false
-
-		factory :admin_user do
-			admin true
-		end
+	end
+	factory :admin_user do
+		email
+		password "123456789"
+		first_name "Admin"
+		last_name "User"
+		admin false
 	end	
 end
--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -5,8 +5,4 @@ end
 # RSpec without Rails
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
-
-  config.before(:suite) do
-    FactoryGirl.find_definitions
-  end
 end
